b12876506b5be59e5af62d7cb8c6691919f7c1fd
Edith Wanja <edithwanjohi254@gmail.com>
2025-07-15 13:41:10 +0300

    Add advance challenge 10-factorial.js and update README.md

---
+++ 10-factorial.js
@@ -0,0 +1,13 @@
+const arg = process.argv[2];
+const number = Number(arg);
+
+function factorial(n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+  else{
+  return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(number));
